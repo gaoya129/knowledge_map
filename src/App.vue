00a45fc13cb96144efc9c4f5226e69a978f2f1ff
gaoya129 <1528824607@qq.com>
2022-01-04 14:12:15 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,147 @@
+<template>
+  <div id="main-page">
+    <el-row :gutter="16">
+      <el-col id="left-part" :span="3.5">
+        <div style="display:flex;flex-direction:right;width:100%">
+          <img class="logo" src="./assets/logo.png">
+          <p style="color:#1976D2;font-size:x-large;font-weight:bold;margin:0;padding-top:10px">地理知识图谱</p>
+        </div>
+        <p style="font-size:xx-large;margin:0;padding-left:20px;">Hello,TJUer</p>
+        <button class="home-btn">
+          <div style="width:100%;display:flex;justify-content:center;">
+            <img src="./assets/home.png" style="vertical-align:middle;width:30px;height:30px;margin-left:-20px">
+            <div style="height:30px;line-height:30px;padding-left:30px">主页</div>
+          </div>
+          </button>
+        <div class="card place-card">
+          <p style="font-size:large;padding:20px;font-weight:bold;margin:0">常用地点</p>
+        </div>
+      </el-col>
+      <el-col id="center-part" :span="15">
+        <div class="card search-card">
+          <div style="display:flex;flex-direction:right;width:100%">
+            <img src="./assets/search.png" style="margin-left:20px;width:30px;height:30px;">
+            <input type="text" style="border:0;outline:none;font-size:medium;margin-left:20px;width:100%" placeholder="请输入需要搜索的内容...">
+          </div>
+          <div style="display:flex;flex-direction:right;height: 100%;">
+            <button class="search-btn search-btn1" style="">搜题目</button>
+            <button class="search-btn search-btn2" style="">搜百科</button>
+          </div>
+        </div>
+        <div class="card map-card" >
+        </div>
+      </el-col>
+      <el-col id="right-part" :span="4">
+        <div class="card result-card" >
+          <p style="font-size:large;padding:20px;font-weight:bold;margin:0">资料卡片</p>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body{
+  background-color: #f9fafc;
+}
+button{
+  border: 0;
+  border-radius:10px;
+}
+input::-webkit-input-placeholder {
+  color: #999;
+  -webkit-transition: color.5s;
+}
+input:focus::-webkit-input-placeholder, input:hover::-webkit-input-placeholder {
+  color: #c2c2c2;
+  -webkit-transition: color.5s;
+}
+#left-part{
+  margin-left: 20px;
+  padding-left: 20px;
+}
+#center-part{
+  margin: 20px;
+  margin-top: 0px;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.home-btn{
+  margin-left:10px;
+  margin-top:30px;
+  width:100%;
+  font-size:large;
+  background-color:#1976D2;
+  color:white;
+  height:40px;
+  box-shadow: 0 4px 8px 0 rgba(156, 156, 156, 0.2), 0 6px 10px 0 rgba(143, 142, 142, 0.19);
+}
+.search-btn{
+  width:100px;
+  height: 100%;
+  color: white;
+  font-size: medium;
+  border-radius: 0rem;
+  box-shadow:0;
+}
+.search-btn1{
+  background-color: #87CEFA;
+}
+.search-btn2{
+  background-color: #4682B4;
+  border-radius: 0 10px 10px 0 ;
+}
+.card{
+  width: 100%;
+  box-shadow: 0 4px 8px 0 rgba(156, 156, 156, 0.2), 0 6px 10px 0 rgba(143, 142, 142, 0.19);
+  border-radius: 10px;
+}
+.place-card{
+  margin-top:30px;
+  margin-left: 10px;
+  min-height: 400px;
+  background-color: white;
+}
+.search-card{
+  margin-top: 10px;
+  height: 50px;
+  background-color: white;
+  display:flex;
+  align-content:center;
+  align-items:center;
+  justify-content: space-between;
+}
+.map-card{
+  background-color: white;
+  height: 100%;
+  min-height:500px;
+  margin-top:40px;
+}
+.result-card{
+  background-color: white;
+  width: 100%;
+  min-height:600px;
+}
+.logo{
+  height: 60px;
+  width: 60px;
+}
+
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+
+</style>
