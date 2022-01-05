30db7bd629bf4ea1c31246725f92255364ec70ed
gaoya129 <1528824607@qq.com>
2022-01-05 13:54:51 +0800

    将高德地图API换换为百度地图

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
+/* eslint-disable no-unused-vars */
 <template>
-  <div id="main-page">
+  <div id="app">
     <el-row :gutter="16">
       <el-col id="left-part" :span="3.5">
         <div style="display:flex;flex-direction:right;width:100%">
@@ -14,7 +15,7 @@
           </div>
           </button>
         <div class="card place-card">
-          <p style="font-size:large;padding:20px;font-weight:bold;margin:0">常用地点</p>
+          <p style="font-size:large;padding:20px;font-weight:bold;margin:0;text-align: left;">常用地点</p>
           <div id="place-label" style="margin-left:20px;width:100%;display:flex;justify-content:flex-start;">
             <img src="./assets/place.png" style="vertical-align:middle;width:30px;height:30px;">
             <div style="height:30px;line-height:30px;padding-left:10px">北京</div>
@@ -25,11 +26,11 @@
         <div class="card search-card">
           <div style="display:flex;flex-direction:right;width:100%">
             <img src="./assets/search.png" style="margin-left:20px;width:30px;height:30px;">
-            <input type="text" style="border:0;outline:none;font-size:medium;margin-left:20px;width:100%" placeholder="请输入需要搜索的内容...">
+            <input type="ques-text" style="border:0;outline:none;font-size:medium;margin-left:20px;width:100%" placeholder="请输入需要搜索的内容...">
           </div>
           <div style="display:flex;flex-direction:right;height: 100%;">
-            <button class="search-btn search-btn1" style="">搜题目</button>
-            <button class="search-btn search-btn2" style="">搜百科</button>
+            <button class="search-btn search-btn1" id="search_ques">搜题目</button>
+            <button class="search-btn search-btn2" @click="searchWiki">搜百科</button>
           </div>
         </div>
         <div class="card map-card">
@@ -38,76 +39,120 @@
       </el-col>
       <el-col id="right-part" :span="4">
         <div class="card result-card" >
-          <p style="font-size:large;padding:20px;font-weight:bold;margin:0">资料卡片</p>
-          <div id="place-info" style="margin-left:20px;">
-            <p style="margin:0">经纬度:</p>
+          <p style="font-size:large;padding:20px;font-weight:bold;margin:0;text-align: left;">资料卡片</p>
+          <div id="place-info" style="margin-left:10px;">
+            <p style="margin:0;margin-left:10px;text-align: left;">经纬度:</p>
             <input id="lnglat" value="111" readonly="readonly" style="outline:none;border:0;width:90%">
+            <p style="margin:0;margin-left:10px;text-align: left;">地区:</p>
             <input id="address" value="222" readonly="readonly" style="outline:none;border:0;width:90%">
             <input id="type" value="333" readonly="readonly" style="outline:none;border:0;width:90%">
           </div>
-          <iframe style="display:none"></iframe>
+          <div style="height:100%">
+            <iframe id="result-page" src="" style="scrolling: no;"></iframe>
+          </div>
         </div>
       </el-col>
     </el-row>
   </div>
 </template>
 
-<script type="text/javascript" src="https://webapi.amap.com/maps?v=2.0&key=bfb4efeff9bc386017a868570f5672aa"></script>
-<script type="text/javascript" src="https://cache.amap.com/lbs/static/addToolbar.js"></script>
 <script type="text/javascript">
-import AMapLoader from '@amap/amap-jsapi-loader';
-AMapLoader.load({
-    "key": "bfb4efeff9bc386017a868570f5672aa",              // 申请好的Web端开发者Key，首次调用 load 时必填
-    "version": "2.0",   // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
-    "plugins": ['AMap.ToolBar','AMap.Scale','AMap.Geocoder'],           // 需要使用的的插件列表，如比例尺'AMap.Scale'等
-}).then((AMap)=>{
-    var map = new AMap.Map('container',{
-      zoom: 0,
-      center: [116.39,39.9],//new AMap.LngLat(116.39,39.9)
-      isHotspot: true,
-      resizeEnable: true,
-    });
-    var toolBar = new AMap.ToolBar();
-    var scale = new AMap.Scale();
-    var marker = new AMap.Marker();;
-    var geocoder = new AMap.Geocoder({
-      city: "010", //城市设为北京，默认：“全国”
-      radius: 1000
-    });
-    // 使用鼠标工具，在地图上画标记点
-    map.addControl(toolBar);
-    map.addControl(scale);
-    function regeoCode(e) {
-      var lnglat  = document.getElementById('lnglat').value.split(',');
-      map.add(marker);
-      marker.setPosition(lnglat);
-      console.log(lnglat)
-      geocoder.getAddress(lnglat, function(status, result) {
-          console.log(status)
-          console.log(result)
-          console.log(result.regeoCode)
-          if (status === 'complete'&&result.regeocode) {
-              var address = result.regeocode.formattedAddress;
-              document.getElementById('address').value = address;
-          }else{
-              console.log('根据经纬度查询地址失败')
-          }
-      });
+// import AMapLoader from '@amap/amap-jsapi-loader'
+import { BMPGL } from '@/bmp.js'
+export default{
+  name: 'app',
+  data () {
+    return {
+      ak: 'IRqbkLBBgG6Amxrb9CKhgUWzl8HY1fs7', // 百度的地图密钥
+      myMap: ''
     }
-    map.on('click', function(e) {
-        document.getElementById("lnglat").value = e.lnglat;
-        console.log(e);
-        // document.getElementById("position").display = visible;
-        regeoCode(e);
-    });
-}).catch(e => {
-    console.log(e);
-})
+  },
+  methods: {
+    searchWiki () {
+      var ques = document.getElementById('ques-text').value
+      console.log(ques)
+      document.getElementById('result-page').src = 'https://wanweibaike.net/wiki-' + ques
+      console.log(document.getElementById('result-page').src)
+      document.getElementById('result-page').style.display = 'block'
+      console.log(document.getElementById('result-page').display)
+    },
+    initMap () {
+      // 传入密钥获取地图回调。
+      BMPGL(this.ak).then((BMapGL) => {
+        // 创建地图实例
+        let map = new BMapGL.Map('container')
+        // 创建点坐标 axios => res 获取的初始化定位坐标
+        let point = new BMapGL.Point(114.031761, 22.542826)
+        // 初始化地图，设置中心点坐标和地图级别
+        map.centerAndZoom(point, 1)
+        // 开启鼠标滚轮缩放
+        map.enableScrollWheelZoom(true)
+        // 保存数据
+        // this.myMap = map
+      })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  },
+  mounted () {
+    this.initMap()
+    // AMapLoader.load({
+    //   'key': 'bfb4efeff9bc386017a868570f5672aa', // 申请好的Web端开发者Key，首次调用 load 时必填
+    //   'version': '2.0', // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+    //   'plugins': ['AMap.ToolBar', 'AMap.Scale', 'AMap.Geocoder'] // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+    // }).then((AMap) => {
+    //   var map = new AMap.Map('container', {
+    //     zoom: 0,
+    //     center: [116.39, 39.9], // new AMap.LngLat(116.39,39.9)
+    //     isHotspot: true,
+    //     resizeEnable: true
+    //   })
+    //   var toolBar = new AMap.ToolBar()
+    //   var scale = new AMap.Scale()
+    //   var marker = new AMap.Marker()
+    //   var geocoder = new AMap.Geocoder({
+    //     city: '010', // 城市设为北京，默认：“全国”
+    //     radius: 1000
+    //   })
+    //   // 使用鼠标工具，在地图上画标记点
+    //   map.addControl(toolBar)
+    //   map.addControl(scale)
+    //   function regeoCode (e) {
+    //     var lnglat = document.getElementById('lnglat').value.split(',')
+    //     map.add(marker)
+    //     marker.setPosition(lnglat)
+    //     console.log(lnglat)
+    //     geocoder.getAddress(lnglat, function (status, result) {
+    //       console.log(status)
+    //       console.log(result)
+    //       console.log(result.regeoCode)
+    //       if (status === 'complete' && result.regeocode) {
+    //         var address = result.regeocode.formattedAddress
+    //         document.getElementById('address').value = address
+    //       } else {
+    //         console.log('根据经纬度查询地址失败')
+    //       }
+    //     })
+    //   }
+    //   map.on('click', function (e) {
+    //     document.getElementById('lnglat').value = e.lnglat
+    //     console.log(e)
+    //     // document.getElementById("position").display = visible;
+    //     regeoCode(e)
+    //   })
+    // }).catch(e => {
+    //   console.log(e)
+    // })
+  }
+
+}
+
 </script>
 
 <style>
 body{
-  background-color: #f9fafc;
+  background-color: #f7f7f7;
 }
 button{
   border: 0;
@@ -120,6 +165,14 @@ input::-webkit-input-placeholder {
 input:focus::-webkit-input-placeholder, input:hover::-webkit-input-placeholder {
   color: #c2c2c2;
   -webkit-transition: color.5s;
+}
+#result-page{
+  margin-top:20px;
+  transform-origin: left top;
+  transform: scale(0.75,1);
+  width: 400px;
+  min-height: 400px;
+  border: 0;
 }
 #container{
   margin: 0px;
